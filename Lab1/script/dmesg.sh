96b6025e36d30d5f581d542c1f344698d74df83c
louisLaporte <louis.laporte12@gmail.com>
2016-10-13 16:26:03 -0700

    script to color watch dmesg

---
+++ Lab1/script/dmesg.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+declare RED=$(printf '\033[31m')
+declare GRN=$(printf '\033[32m')
+declare YEL=$(printf '\033[33m')
+declare BLU=$(printf '\033[34m')
+declare MAG=$(printf '\033[35m')
+declare CYA=$(printf '\033[36m')
+declare RST=$(printf '\033[0m')
+#echo "scull_read" | sed "s/scull_read/${esc}[32m&${esc}[0m/"
+
+
+watch --color "dmesg | tail -n 50 | sed \"
+                        s/scull_read/${GRN}&${RST}/
+                        s/scull_write/${YEL}m&${RST}/
+                        s/scull_release/${MAG}m&${RST}/
+                        s/scull_init/${CYA}m&${RST}/
+                        \""
