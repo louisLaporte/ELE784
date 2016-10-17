7a485f9d1bab1f897a2220a9f738f5b650ef8306
louisLaporte <louis.laporte12@gmail.com>
2016-10-16 19:18:59 -0700

    update init and access

--- Lab1/src/module/scull.c
+++ Lab1/src/module/scull.c
@@ -1,4 +1,5 @@
 #include "scull.h"
+
 struct file_operations scull_fops = {
 	.owner	        =	THIS_MODULE,
 	.read		=	scull_read,
@@ -7,65 +8,98 @@ struct file_operations scull_fops = {
 	.release	=	scull_release
 };
 
-int BufIn(struct ring_buffer *Buf, unsigned char *Data) {
-  if (Buf->BufFull)
+int buf_in(struct ring_buffer *buf, unsigned char *data) {
+  if (buf->buf_full)
     return -1;
-  Buf->BufEmpty = 0;
-  Buf->Buffer[Buf->InIdx] = *Data;
-  Buf->InIdx = (Buf->InIdx + 1) % Buf->BufSize;
-  if (Buf->InIdx == Buf->OutIdx)
-    Buf->BufFull = 1;
+  buf->buf_empty = 0;
+  buf->buffer[buf->in_idx] = *data;
+  buf->in_idx = (buf->in_idx + 1) % buf->buf_size;
+  if (buf->in_idx == buf->out_idx)
+    buf->buf_full = 1;
   return 0;
 }
 
-int BufOut(struct ring_buffer *Buf, unsigned char *Data) {
-        if (Buf->BufEmpty)
+int buf_out(struct ring_buffer *buf, unsigned char *data) {
+        if (buf->buf_empty)
                 return -1;
-        Buf->BufFull = 0;
-        *Data = Buf->Buffer[Buf->OutIdx];
-        Buf->OutIdx = (Buf->OutIdx + 1) % Buf->BufSize;
-        if (Buf->OutIdx == Buf->InIdx)
-                Buf->BufEmpty = 1;
+        buf->buf_full = 0;
+        *data = buf->buffer[buf->out_idx];
+        buf->out_idx = (buf->out_idx + 1) % buf->buf_size;
+        if (buf->out_idx == buf->in_idx)
+                buf->buf_empty = 1;
         return 0;
 }
 
 
 int scull_open(struct inode *inode, struct file *filp) {
-        printk(KERN_WARNING"scull_open (%s:%u)\n", __FUNCTION__, __LINE__);
+
+        printk(KERN_WARNING"scull_open\n");
+
+        switch ((filp->f_flags & O_ACCMODE)) {
+        case O_RDONLY:
+                printk("Read only\n");
+                scull_dev.num_reader++;
+                break;
+        case O_WRONLY:
+                printk("Write only\n");
+                if(scull_dev.num_writer > 0)
+                        return -ENOTTY; /* Not a typewriter */
+                scull_dev.num_writer = MAX_WRITER;
+                break;
+        case O_RDWR:
+                printk("Read and write\n");
+                if(scull_dev.num_writer > 0)
+                        return -ENOTTY; /* Not a typewriter */
+                scull_dev.num_writer = MAX_WRITER;
+                break;
+        default:
+                break;
+        }
+        printk(KERN_WARNING"nb: reader = %d | writer = %d ", 
+                        scull_dev.num_reader, scull_dev.num_writer);
         return 0;
 }
 
 int scull_release(struct inode *inode, struct file *filp) {
+        
+        printk(KERN_WARNING"scull_release\n");
 
-  //      switch(filp->f_flags & O_ACCMODE) {
-  //          case O_RDONLY:
-  //                  s.numReader--;
-  //                  break;
-  //          case O_WRONLY:
-  //                  s.numWriter--;
-  //                  break;
-  //          case O_RDWR:
-  //                  s.numWriter--;
-  //                  break;
-  //          default:
-  //                  break;
-  //      }
-//        printk(KERN_WARNING"%snb: reader = %d | writer = %d ", 
-//                , __FUNCTION__, s.numWriter, s.numReader);
-
+        switch(filp->f_flags & O_ACCMODE) {
+        case O_RDONLY:
+                scull_dev.num_reader--;
+                break;
+        case O_WRONLY:
+                scull_dev.num_writer--;
+                break;
+        case O_RDWR:
+                scull_dev.num_writer--;
+                break;
+        default:
+                break;
+        }
+        printk(KERN_WARNING"nb: reader = %d | writer = %d ", 
+                        scull_dev.num_reader, scull_dev.num_writer);
         return 0;
 }
 
-static ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) {
+static ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+                loff_t *f_pos) 
+{
+        ssize_t result = 0;
         char ch;
 
         if (num > 0) {
                 ch = tampon[--num];
-/***
- * Copy data from kernel space to user space.
- * Returns number of bytes that could not be copied. On success, this will be zero.
- */
-                copy_to_user(buf, &ch, 1);
+                /*
+                 * Copy data from kernel space to user space.
+                 * Returns number of bytes that could not be copied.
+                 * On success, this will be zero.
+                 */
+                if(copy_to_user(buf, &ch, 1)) {
+                        result = -EFAULT; /* Bad address */
+                        goto out;
+                }
+
                 printk(KERN_WARNING"scull_read (%s:%u) count = %lu ch = %c\n",
                         __FUNCTION__, __LINE__, count, ch);
                 return 1;
@@ -74,77 +108,147 @@ static ssize_t scull_read(struct file *filp, char __user *buf, size_t count, lof
                         __FUNCTION__, __LINE__, count);
                 return 0;
         }
+out:
+        return result;
+
 }
 
-static ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos) {
+static ssize_t scull_write(struct file *filp, const char __user *buf, 
+                size_t count, loff_t *f_pos) 
+{
+        //ssize_t result = -ENOMEM;
+        ssize_t result = 0;
 	char ch;
         if (num < 10) {
-/*
- * Copy data from user space to kernel space.
- * Returns number of bytes that could not be copied. On success, this will be zero.
- * If some data could not be copied, this function will pad the copied data to the requested size using zero bytes.
- */
-                copy_from_user(&ch, buf, 1);
+                /*
+                 * Copy data from user space to kernel space.
+                 * Returns number of bytes that could not be copied. 
+                 * On success, this will be zero.
+                 * If some data could not be copied, this function will pad 
+                 * the copied data to the requested size using zero bytes.
+                 */
+                if(copy_from_user(&ch, buf, 1)) {
+                        result = -EFAULT; /* Bad address */
+                        goto out;
+                }
                 tampon[num++] = ch;
-                printk(KERN_WARNING"scull_write (%s:%u) count = %lu ch = %c\n", __FUNCTION__, __LINE__, count, ch);
+                printk(KERN_WARNING"scull_write (%s:%u) count = %lu ch = %c\n",
+                        __FUNCTION__, __LINE__, count, ch);
                 return 1;
         } else {
-                printk(KERN_WARNING"scull_write (%s:%u) count = %lu ch = no place\n", __FUNCTION__, __LINE__, count);
+                printk(KERN_WARNING"scull_write (%s:%u) count = %lu ch = no place\n",
+                        __FUNCTION__, __LINE__, count);
                 return 0;
         }
+out:
+        return result;
 }
 
 
+
+/*
+ * TODO: Validate IS_ERR and PTR_ERR in err.h
+ *       are same to if(!ptr){res = -ENOMEM; goto err_ptr} 
+ */
 static int __init scull_init (void) {
-        int result;
 
-        printk(KERN_ALERT"scull_init (%s:%u) => Hello, World !!!\n",
-                __FUNCTION__, __LINE__);
-/*
- * Allocates a range of char device numbers. The major number will be chosen dynamically,
- * and returned (along with the first minor number) in dev. 
- * Returns zero or a negative error code.
- */
-        result = alloc_chrdev_region(&s.dev, 0, 1, "scull");
-        if (result < 0)
-                printk(KERN_WARNING"scull_init ERROR IN alloc_chrdev_region (%s:%s:%u)\n", __FILE__, __FUNCTION__, __LINE__);
-        else
-                printk(KERN_WARNING"scull_init : MAJOR = %u MINOR = %u (scull_var = %u)\n", MAJOR(s.dev), MINOR(s.dev), scull_var);
-/*
- * This is used to create a struct class pointer that can then be used in calls to class_device_create.
- * Note, the pointer created here is to be destroyed when finished by making a call to class_destroy.
- */
-        c = class_create(THIS_MODULE, "scullClass");
-/*
- * A struct device will be created in sysfs, registered to the specified class.
- * 
- * A “dev” file will be created, showing the dev_t for the device, if the dev_t is not 0,0.
- * If a pointer to a parent struct device is passed in,
- * the newly created struct device will be a child of that device in sysfs.
- * The pointer to the struct device will be returned from the call.
- * Any further sysfs files that might be required can be created using this pointer.
- */
-        device_create(c, NULL, s.dev, NULL, "scull_Node");
-/*
- * Initializes cdev, remembering fops, making it ready to add to the system with cdev_add.
- */
-        cdev_init(&s.cdev, &scull_fops);
-        s.cdev.owner = THIS_MODULE;
-        if (cdev_add(&s.cdev, s.dev, 1) < 0)
-                printk(KERN_WARNING"scull ERROR IN cdev_add (%s:%s:%u)\n",
-                            __FILE__, __FUNCTION__, __LINE__);
+        int result = 0;
 
-    return 0;
+        printk(KERN_ALERT"scull_init");
+        /* ring buffer initialization */
+        ring_buf.buffer = (unsigned short*) 
+            kmalloc(DEFAULT_BUFSIZE * sizeof(unsigned short), GFP_KERNEL);
+        if(IS_ERR(ring_buf.buffer))
+                return ERR_PTR(-ENOMEM); /* Out of memory */
+        ring_buf.buf_empty = 1;
+        ring_buf.buf_full  = 0;
+        ring_buf.buf_size  = DEFAULT_BUFSIZE;
+        ring_buf.in_idx    = 0;
+        ring_buf.out_idx   = 0;
+        /* scull initialization */
+        scull_dev.read_buf = (unsigned short *) 
+                kmalloc(DEFAULT_RWSIZE * sizeof(unsigned short), GFP_KERNEL);
+        if(IS_ERR(scull_dev.read_buf))
+                return ERR_PTR(-ENOMEM); /* Out of memory */
+        scull_dev.write_buf = (unsigned short *) 
+                kmalloc(DEFAULT_RWSIZE * sizeof(unsigned short), GFP_KERNEL);
+        if(IS_ERR(scull_dev.write_buf))
+                return ERR_PTR(-ENOMEM); /* Out of memory */
+        scull_dev.num_writer = 0;
+        scull_dev.num_reader = 0;
+        /*
+         * Allocates a range of char device numbers. 
+         * The major number will be chosen dynamically and returned 
+         * (along with the first minor number) in dev. 
+         * Returns zero or a negative error code.
+         */
+        result = alloc_chrdev_region(&scull_dev.dev, 0, 1, "scull");
+        if(result < 0) {
+                printk(KERN_ERR "failed to alloc chrdev region\n");
+                goto fail_alloc_chrdev_region;
+        }
+        /*
+         * This is used to create a struct class pointer that can then be used 
+         * in calls to class_device_create.
+         * Note, the pointer created here is to be destroyed when finished
+         * by making a call to class_destroy.
+         */
+        class_dev = class_create(THIS_MODULE, "scull_class");
+        if(!class_dev) {
+                result = -EEXIST; /* File exists */
+                printk(KERN_ERR "failed to create class\n");
+                goto fail_create_class;
+        }
+        /*
+         * A struct device will be created in sysfs, registered to the specified
+         * class.
+         * 
+         * A “dev” file will be created, showing the dev_t for the device,
+         * if the dev_t is not 0,0.
+         * If a pointer to a parent struct device is passed in, the newly 
+         * created struct device will be a child of that device in sysfs.
+         * The pointer to the struct device will be returned from the call.
+         * Any further sysfs files that might be required can be created using 
+         * this pointer.
+         */
+        if(!(device_create(class_dev, NULL, scull_dev.dev, NULL, "scull_Node"))) {
+                result = -EINVAL; /* Invalid argument */
+                printk(KERN_ERR "failed to create device\n");
+                goto fail_create_device;
+        }
+        /*
+         * Initializes cdev, remembering fops, making it ready to add 
+         * to the system with cdev_add.
+         */
+        cdev_init(&scull_dev.cdev, &scull_fops);
+        scull_dev.cdev.owner = THIS_MODULE;
+        if (cdev_add(&scull_dev.cdev, scull_dev.dev, 1) < 0)
+                printk(KERN_WARNING"scull ERROR IN cdev_add\n");
+        return 0;
+
+fail_create_device:
+        class_destroy(class_dev);
+fail_create_class:
+        cdev_del(&scull_dev.cdev);
+fail_alloc_chrdev_region:
+        kfree(ring_buf.buffer);
+        kfree(scull_dev.read_buf);
+        kfree(scull_dev.write_buf);
+        return result;
 }
 
 static void __exit scull_exit (void) {
-        cdev_del(&s.cdev);
-        unregister_chrdev_region(s.dev, 1);
-        device_destroy (c, s.dev);
-        class_destroy(c);
+        /* free pointers */
+        kfree(ring_buf.buffer);
+        kfree(scull_dev.read_buf);
+        kfree(scull_dev.write_buf);
+        /* remove device and class device */       
+        cdev_del(&scull_dev.cdev);
+        unregister_chrdev_region(scull_dev.dev, 1);
+        device_destroy (class_dev, scull_dev.dev);
+        class_destroy(class_dev);
 
-        printk(KERN_ALERT"scull_exit (%s:%u) => Goodbye, cruel world\n",
-                __FUNCTION__, __LINE__);
+        printk(KERN_ALERT"scull_exit");
 }
 
 module_init(scull_init);
--- Lab1/src/module/scull.h
+++ Lab1/src/module/scull.h
@@ -6,6 +6,9 @@
 #include <linux/cdev.h>
 #include <asm/uaccess.h>    /* copy_*_user */
 #include <asm/atomic.h>
+#include <linux/errno.h>
+#include <linux/err.h>
+#include <linux/slab.h>
 
 
 MODULE_LICENSE("Dual BSD/GPL");
@@ -13,35 +16,40 @@ MODULE_LICENSE("Dual BSD/GPL");
 /************************ Définitions ********************************/
 #define DEFAULT_RWSIZE 16
 #define DEFAULT_BUFSIZE 256
-#define MAX_USER 10
+#define MAX_WRITER 1
 /***************************** Structures ************************************/
 struct ring_buffer {
-    unsigned int    InIdx   ;
-    unsigned int    OutIdx  ;
-    unsigned short  BufFull ;
-    unsigned short  BufEmpty;
-    unsigned int    BufSize ;
-    char            *Buffer ;
+    unsigned int    in_idx   ;
+    unsigned int    out_idx  ;
+    unsigned short  buf_full ;
+    unsigned short  buf_empty;
+    unsigned int    buf_size ;
+    unsigned short  *buffer  ;
 };
 struct scull {
-    char            *ReadBuf    ;
-    char            *WriteBuf   ;
-    unsigned short  numWriter   ;
-    unsigned short  maxReader   ;
-    dev_t           dev         ;
-    struct cdev     cdev        ;
+    unsigned short   *read_buf    ;
+    unsigned short   *write_buf   ;
+    //struct semaphore sem_buf     ;
+    unsigned short   num_writer   ;
+    unsigned short   num_reader   ;
+    dev_t            dev          ;
+    struct cdev      cdev         ;
 };
+
+struct scull scull_dev;
+struct ring_buffer ring_buf;
+struct class *class_dev;
 
 int scull_open(struct inode *inode, struct file *filp);
 int scull_release(struct inode *inode, struct file *filp);
-static ssize_t scull_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos);
-static ssize_t scull_write(struct file *filp, const char __user *buf, size_t count, loff_t *f_pos);
+static ssize_t scull_read(struct file *filp, char __user *buf, size_t count,
+                loff_t *f_pos);
+static ssize_t scull_write(struct file *filp, const char __user *buf,
+                size_t count, loff_t *f_pos);
 
 static int   scull_init (void);
 static void  scull_exit (void);
-struct scull s;
-struct ring_buffer r;
-struct class *c;
+
 
 int scull_var = 0;
 module_param(scull_var, int, S_IRUGO);
